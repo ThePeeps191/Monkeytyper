668d355a0cb6a11137475ecfb9f21323eadbe1c4
Danny Wang (ThePeeps191) <74725787+ThePeeps191@users.noreply.github.com>
2024-02-29 13:36:31 -0700

    added press alt key to break

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ from PIL import Image
 import pyscreenshot as ImageGrab
 import numpy as np
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd =r"C:\Users\thepe\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
+import keyboard
+from tkinter import messagebox
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\thepe\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 time.sleep(5)
 while True:
     region_screenshot = ImageGrab.grab(bbox=(119, 458, 1671, 546))
     region_screenshot.save('region_screenshot.png')
-    img2 = np.array(Image.open("region_screenshot.png"))
-    text = pytesseract.image_to_string(img2)
+    img = np.array(Image.open("region_screenshot.png"))
+    text = pytesseract.image_to_string(img)
     pyautogui.write(text, interval=0.017)
     pyautogui.press("enter")
     time.sleep(2)
+    if keyboard.is_pressed("alt"):
+        messagebox.showinfo("Stopped Running", "Code stopped running because of pressed ALT key.")
+        break
     pyautogui.click(x=322, y=257)
     time.sleep(1)
